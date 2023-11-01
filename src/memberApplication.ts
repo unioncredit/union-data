8f4676cc9ec7eeeea31a2c26cdbcb7cfb8d930bf
0xbarbs <0xbarbs@proton.me>
2023-11-01 13:35:56 +0000

    feat: added fetchAccountMembershipApplication function

--- src/memberApplication.ts
+++ src/memberApplication.ts
@@ -1,6 +1,7 @@
 import { gql } from "graphql-request";
 import { OrderDirection } from "./constants";
 import { fetchPages } from "./fetchPages";
+import { objectToWhere } from "./utils";
 
 interface MemberApplication {
   id: string;
@@ -17,13 +18,20 @@ interface MemberApplication {
  */
 export async function fetchMemberApplications(
   orderBy: string = "timestamp",
-  orderDirection: OrderDirection = OrderDirection.DESC
+  orderDirection: OrderDirection = OrderDirection.DESC,
+  where?: { [key: string]: string }
 ): Promise<MemberApplication[]> {
+  const whereStr = objectToWhere(where);
+
   const query = (skip: number, first: number) => gql`
     {
-      memberApplications(skip: ${skip}, first: ${first}, orderBy: ${orderBy}, orderDirection: ${
-    orderDirection === OrderDirection.ASC ? "asc" : "desc"
-  }) {
+      memberApplications(
+        skip: ${skip},
+        first: ${first},
+        orderBy: ${orderBy},
+        orderDirection: ${orderDirection === OrderDirection.ASC ? "asc" : "desc"}
+        ${whereStr}
+      ) {
         id
         staker
         applicant
@@ -33,4 +41,19 @@ export async function fetchMemberApplications(
   `;
 
   return fetchPages<MemberApplication>(query, "memberApplications");
+}
+
+/**
+ * Get a single members borrow historical data
+ * @param {string} account - Account to get borrow historical data for
+ * @param {string} orderBy - Property to orderBy
+ * @param {OrderDirection} orderDirection - Order in asc or desc
+ * @returns {Promise} Promise of `{ id, account, amount, fee, timestamp }[]`
+ */
+export async function fetchAccountMembershipApplication(
+  account: string,
+  orderBy: string = "timestamp",
+  orderDirection: OrderDirection = OrderDirection.DESC
+): Promise<MemberApplication[]> {
+  return fetchMemberApplications(orderBy, orderDirection, { applicant: account });
 }
